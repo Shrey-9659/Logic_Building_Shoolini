e64cee61f6cac08b9c51d3b78b73e687549d8a2a
Shrey Khandelwal <shrey.khandelwal.2000@gmail.com>
2025-09-16 19:33:36 +0530

    input output

---
+++ G4P/questions.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+// int main(){
+//     int a = 5;
+//     int b = 15;
+//     int c = a + b;
+//     printf("The sum of %d and %d is %d", a,b,c);
+//     return 0;
+// }
+
+// Print the area of a circle;
+// int main() {
+//     int radius;
+//     const float pi = 3.14;
+//     printf("Enter the value of radius : \n");
+//     scanf("%d", &radius);
+//     float area = pi * (radius * radius);
+//     printf("The area of circle with radius %d is %.2f", radius, 
+//         area);
+//     return 0;
+// }
+// Take 2 input from the user, and print the area of a rectangle
+// int main(){
+//     int length, width;
+//     printf("Enter the length : ");
+//     scanf("%d", &length);
+//     printf("Enter the width : ");
+//     scanf("%d", &width);
+//     printf("The area of rectangle with sides %d and %d is %d",
+//     length, width, length*width);
+//     return 0;
+// }
---
+++ intro.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+// int main(){
+//     printf("Hello World!");
+//     return 0;
+// }
+
+// gcc fileName.c -o fileName
+// ./fileName
+
+int main(){
+    int varA = 5;
+    int varB = 10;
+    printf("The number is %d and %d", varA, varB);
+// The numbers are 5 and 10
+    return 0;
+}
