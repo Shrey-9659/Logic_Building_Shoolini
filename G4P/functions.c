e1e2f21fe24de314d2ea978f444fc8e8d43e4a0b
Shrey Khandelwal <shrey.khandelwal.2000@gmail.com>
2025-11-04 20:34:37 +0530

    upd

--- G4P/functions.c
+++ G4P/functions.c
@@ -1,23 +1,50 @@
 #include <stdio.h>
 
-void myName(){
-    printf("My name is Shrey Khandelwal");
-}
-int sumTwoValue(int a, int b){
-    printf("%d\n", a+b);
-    return 0;
-}
-void evenOrOdd(int num){
-    if(num%2==0){
-        printf("Number is even");
-    }else{
-        printf("Number is odd");
-    }
-}
+// void myName(){
+//     printf("My name is Shrey Khandelwal");
+// }
+// int sumTwoValue(int a, int b){
+//     printf("%d\n", a+b);
+//     return 0;
+// }
+// void evenOrOdd(int num){
+//     if(num%2==0){
+//         printf("Number is even");
+//     }else{
+//         printf("Number is odd");
+//     }
+// }
 
-int main(){
+// int main(){
     // int firstValue = sumTwoValue(5,6);
     // int secondValue = sumTwoValue(8,6);
     // printf("Value of firstValue is : %d", firstValue);
-    evenOrOdd(16);
+//     evenOrOdd(16);
+// }
+
+// int findMax(int a, int b){
+//     if(a>b){
+//         return a;
+//     }else{
+//         return b;
+//     }
+// }
+// int main(){
+//     printf("%d", findMax(10, 8));
+//     return 0;
+// }
+
+// Write a program to check if a number is even or odd using a function
+
+void findEvenOrOdd(int num){
+    if(num % 2 == 0){
+         printf("Given number is even");
+    }else{
+        printf("Given number is odd");
+    }
+}
+int main(){
+    findEvenOrOdd(6);
+    findEvenOrOdd(61);
+    return 0;
 }
---
+++ array.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(){
+    // int marks1 = 35;
+    // int marks2 = 40;
+    // int marks3 = 45;
+    // int marks4 = 50;
+    // int marks5 = 55;
+
+    // int marks[5] = {35, 40, 45, 50, 55};
+    int marks[5] = {35, 40};
+    printf("%d\n", marks[0]);
+    printf("%d\n", marks[1]);
+    printf("%d\n", marks[2]);
+    printf("%d\n", marks[3]);
+    printf("%d\n", marks[4]);
+
+    return 0;
+}
+
+// [35, 40, 45, 50, 55]
